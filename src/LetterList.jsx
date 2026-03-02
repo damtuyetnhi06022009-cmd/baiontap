7b7f5f97407579174bb708437bf7a8cf77b8e0cc
damtuyetnhi06022009-cmd <damtuyetnhi06022009@gmail.com>
2026-03-02 09:13:44 +0700

    Add multiple files

--- src/LetterList.jsx
+++ src/LetterList.jsx
@@ -3,8 +3,8 @@ import LetterItem, { useEffect } from "./Letteritem";
 function LetterList({ letters }) {
     return (
         <div style={{ marginTop: "20px" }}>
-            {letters.length === 0 && <p>Chưa có letter nào</p>}
-            {letters.map((letter) => (
+            {letters && letters.length === 0 && <p>Chưa có letter nào</p>}
+            {letters && letters.map((letter) => (
                 <LetterItem key={letter.id} letter={letter} />
             ))}
         </div>
